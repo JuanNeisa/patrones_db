50d3e168d13c7dd1ee9293c12b0c99a345a06d2f
JuanNeisa <42445731+JuanNeisa@users.noreply.github.com>
2020-07-29 01:17:48 -0500

    Primer commit

---
+++ src/conexion/conexion.java
@@ -0,0 +1,14 @@
+package conexion;
+
+
+/**
+ * Esta clase se encargara de realizar la conexion con el servidor que almacena
+ * los datos de la base de datos del programa
+ *
+ * @author Juan David Neisa, Andres Ramirez y Jorge Escobar
+ */
+public class conexion {
+
+    
+
+}
